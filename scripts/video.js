6ab820a45c45a3fe86a3a7357f58d7cf90b74289
MursalinDevOps <mursalin1768@gmail.com>
2024-10-07 23:29:12 +0600

    categories button added(dynamically)

--- scripts/video.js
+++ scripts/video.js
@@ -1,1 +1,19 @@
-console.log('Hello World!');
+// LOAD CATEGORIES
+const loadCategories = () => {
+    fetch('https://openapi.programming-hero.com/api/phero-tube/categories')
+    .then(res=>res.json())
+    .then(data=>displayCategories(data.categories))
+    .catch((err)=>(console.log(err)))
+}
+// DISPLAY CATEGORIES
+const displayCategories = (categories) => {
+    categories.forEach(item => {
+        const categoryContainer = document.getElementById('category-container')
+        // console.log(item.category);
+        const button = document.createElement('button');
+        button.innerText = `${item.category}`;
+        button.classList.add('btn','bg-[#FF1F3D]','text-white','w-[100px]');
+        categoryContainer.appendChild(button)
+    });
+}
+loadCategories();
